857ba78368ace710db2d254ea25de320547c14fc
GAARA TECH <berthochery4@gmail.com>
2025-06-09 20:32:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   }
 });
 
@@ -42,6 +42,7 @@ io.on('connection', socket => {
   socket.emit('message', 'Connexion au serveur...');
 });
 
-server.listen(3000, () => {
-  console.log('Serveur lancé sur http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
